e7308bfb4cfe49b4b8cc780a103c807f747c811c
Louis Disap <louis.dispa@insa-rouen.fr>
2021-05-03 15:10:16 +0200

    amelioration script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,17 @@
 #!/bin/bash 
 
-echo "-> Compiling server..."
-javac -cp class:lib/* -sourcepath src src/ir/mom/server/view/MomService.java -d class/
-echo "-> Compiling client..."
-javac -cp class:lib/* -sourcepath src src/ir/mom/client/MomClientTerminal.java -d class/
+compile_server() {
+    echo "-> Compiling server..."
+    javac -cp class:lib/* -sourcepath src src/ir/mom/server/view/MomService.java -d class/
+}
+
+compile_client() {
+    echo "-> Compiling client..."
+    javac -cp class:lib/* -sourcepath src src/ir/mom/client/MomClientTerminal.java -d class/
+}
+
+case $1 in
+"server") compile_server ;;
+"client") compile_client ;;
+"") compile_server; compile_client;;
+esac
